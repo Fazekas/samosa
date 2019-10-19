f5c73f23645fcd7a63db2d27bc2b29f729e8ae9f
NFazekas <nfazekas@nexient.com>
2019-10-18 20:27:09 -0400

    First commit

---
+++ src/main/java/com/food/samosa/SamosaApplication.java
@@ -0,0 +1,13 @@
+package com.food.samosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamosaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/food/samosa/SamosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.samosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
